9e4cd4debf2c6f099e1dd17b312642cbde51ea39
Marc Murray <marcdm@nyzam>
2016-02-08 19:59:04 -0500

    initial models added

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 from trytond.pool import Pool
-from .models import DiseaseNotification
+from .models import DiseaseNotification, TravelHistory, NotificationSymptom
 
 
 def register():
     Pool.register(
         DiseaseNotification,
+        NotificationSymptom,
+        TravelHistory,
         module='health_disease_notification', type_='model')
 
---
+++ models.py
@@ -0,0 +1,80 @@
+
+from trytond.model import ModelView, ModelSQL, fields
+from trytond.transaction import Transaction
+from trytond.pyson import Eval, Not
+
+ONLY_IF_ADMITTED = {'invisible': ~Eval('hospital_admission')}
+ONLY_IF_LAB = {'invisible': ~Eval('specimen_taken')}
+ONLY_IF_DEAD = {'invisible': ~Eval('deceased')}
+
+REQD_IF_LAB = dict([('required', Eval('specimen_taken'))] + ONLY_IF_LAB.items())
+RO_SAVED = {'readonly': Eval('id', 0)>0}
+NOTIFICATION_STATES = [
+    (None, ''),
+    ('suspected', 'Suspected'),
+    ('confirmed', 'Confirmed'),
+    ('discarded', 'Discarded (confirmed negative)')
+]
+
+
+class DiseaseNotification(ModelView, ModelSQL):
+    'Disease Notification'
+    __name__ = 'gnuhealth.disease_notification'
+
+    patient = fields.Many2One('gnuhealth.patient', 'Patient', required=True,
+                              states=RO_SAVED)
+    status = fields.Selection(NOTIFICATION_STATES, 'Status')
+    name = fields.Char('Code', states=RO_SAVED)
+    date_notified = fields.DateTime('Date reported', required=True,
+                                    states=RO_SAVED)
+    diagnosis = fields.Many2One('gnuhealth.pathology', 'Presumptive Diagnosis',
+                                states=RO_SAVED)
+    symptoms = fields.One2Many('gnuhealth.disease_notification.symptom',
+                               'name', 'Symptoms')
+    date_onset = fields.Date('Date of Onset', states=RO_SAVED)
+    date_seen = fields.Date('Date Seen', states=RO_SAVED)
+    encounter = fields.Many2One('gnuhealth.encounter', 'Clinical Encounter'),
+    specimen_taken = fields.Boolean('Specimen Taken')
+    specimen_type = fields.Char('Type', states=REQD_IF_LAB)
+    specimen_date = fields.Date('Date Speciment Taken', states=REQD_IF_LAB)
+    specimen_lab = fields.Char('Lab sent to', states=REQD_IF_LAB)
+    test_result = fields.Char('Lab test result', states=ONLY_IF_LAB)
+    lab_request = fields.Many2One('gnuhealth.patient.lab.test',
+                                  'Lab Test Request', states=ONLY_IF_LAB)
+    hospital_admission = fields.Boolean('Admitted to hospital')
+    admission_date = fields.Date('Date admitted', states=ONLY_IF_ADMITTED)
+    ward = fields.Char('Ward', states=ONLY_IF_ADMITTED)
+    deceased = fields.Boolean('Deceased')
+    date_of_death = fields.Date('Date of Death', states=ONLY_IF_DEAD)
+    reporting_facility = fields.Many2One('gnuhealth.institution')
+    hx_travel = fields.Boolean('Overseas Travel',
+                               help="History of Overseas travel in the last"
+                               " 4 - 6 weeks")
+    hx_locations = fields.One2Many(
+        'gnuhealth.disease_notification.travel', 'notification',
+        states={'invisible': ~Eval('hx_travel', False),
+                'required': Eval('hx_travel', False)})
+
+    @classmethod
+    def __setup__(cls):
+        cls._order.insert(0, ('date_notified', 'DESC'))
+
+class NotificationSymptom(ModelView, ModelSQL):
+    'Symptom'
+    __name__ = 'gnuhealth.disease_notification.symptom'
+    name = fields.Many2One('gnuhealth.disease_notification', 'Notification')
+    pathology = fields.Many2One'gnuhealth.pathology', 'Sign/Symptom',
+        domain=[('code', 'like', 'R%')], required=True)
+    comments = fields.Char('Comments')
+
+
+class TravelHistory(ModelView, ModelSQL):
+    'Travel History'
+    __name__ = 'gnuhealth.disease_notification.travel'
+    notification = fields.Many2One('gnuhealth.disease_notification', 'Notification')
+    country = fields.Many2One('country.country', 'Country', required=True)
+    subdiv = fields.Many2One('country.subdivision', 'Province/State',
+                             domain=[('country', '=', Eval('country'))],
+                             depends=['country'])
+    departure_date = fields.Date('Date of departure',
+                                  help='Date departed from country visited')
