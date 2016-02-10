3e26e901dbc7b8c0b6ae88450cf3871b13e4e179
Marc Murray <murraym@moh.gov.jm>
2016-02-10 13:26:30 -0500

    inital view setups

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 from trytond.pool import Pool
-from .models import DiseaseNotification, TravelHistory, NotificationSymptom
-
+from .models import (DiseaseNotification, TravelHistory, NotificationSymptom,
+                     NotifiedSpecimen)
 
 def register():
     Pool.register(
         DiseaseNotification,
         NotificationSymptom,
+        NotifiedSpecimen,
         TravelHistory,
         module='health_disease_notification', type_='model')
-
--- models.py
+++ models.py
@@ -8,7 +8,9 @@ ONLY_IF_DEAD = {'invisible': ~Eval('deceased', False)}
 
 REQD_IF_LAB = dict([('required', Eval('specimen_taken', False))] +
                    ONLY_IF_LAB.items())
-RO_SAVED = {'readonly': Eval('id', 0) > 0}
+RO_SAVED = {'readonly': Eval('id', 0) > 0}  # readonly after saved
+RO_NEW = {'readonly': Eval('id', 0) < 0}  # readonly when new
+
 NOTIFICATION_STATES = [
     (None, ''),
     ('suspected', 'Suspected'),
@@ -19,7 +21,7 @@ SPECIMEN_TYPES = [
     ('blood', 'Blood'),
     ('urine', 'Urine'),
     ('sputum', 'Sputum'),
-    ('csf', 'Csf, - Cerebrospinal Fluid'),
+    ('csf', 'Cerebrospinal Fluid (CSF)'),
     ('stool', 'Stool'),
     ('throat swab', 'Throat Swab'),
     ('eye swab', 'Eye Swab'),
@@ -68,10 +70,7 @@ class DiseaseNotification(ModelView, ModelSQL):
         states={'invisible': ~Eval('hx_travel', False),
                 'required': Eval('hx_travel', False)})
 
-    @classmethod
-    def __setup__(cls):
-        super(DiseaseNotification, cls).__setup__()
-        cls._order.insert(0, ('date_notified', 'DESC'))
+    _order = [('date_notified', 'DESC')]
 
 
 class NotifiedSpecimen(ModelSQL, ModelView):
@@ -80,11 +79,13 @@ class NotifiedSpecimen(ModelSQL, ModelView):
     __name__ = 'gnuhealth.disease_notification.specimen'
     notification = fields.Many2One('gnuhealth.disease_notification',
                                    'Notification', required=True)
-    specimen_type = fields.Char('Type', required=True)
-    date_taken = fields.Date('Date Speciment Taken', required=True)
-    lab_sent_to = fields.Char('Lab sent to', required=True)
-    lab_result = fields.Char('Lab test result')
-    date_tested = fields.Date('Date tested')
+    specimen_type = fields.Selection(SPECIMEN_TYPES, 'Type', required=True,
+                                     states=RO_SAVED)
+    date_taken = fields.Date('Date Speciment Taken', required=True,
+                             states=RO_SAVED)
+    lab_sent_to = fields.Char('Lab sent to', required=True, states=RO_SAVED)
+    lab_result = fields.Text('Lab test result', states=RO_NEW)
+    date_tested = fields.Date('Date tested', states=RO_NEW)
     lab_request = fields.Many2One('gnuhealth.patient.lab.test',
                                   'Lab Test Request')
 
@@ -93,7 +94,8 @@ class NotificationSymptom(ModelView, ModelSQL):
     'Symptom'
 
     __name__ = 'gnuhealth.disease_notification.symptom'
-    name = fields.Many2One('gnuhealth.disease_notification', 'Notification')
+    name = fields.Many2One('gnuhealth.disease_notification', 'Notification',
+                           required=True)
     pathology = fields.Many2One('gnuhealth.pathology', 'Sign/Symptom',
                                 domain=[('code', 'like', 'R%')], required=True)
     date_onset = fields.Date('Date of onset')
@@ -105,10 +107,13 @@ class TravelHistory(ModelView, ModelSQL):
 
     __name__ = 'gnuhealth.disease_notification.travel'
     notification = fields.Many2One('gnuhealth.disease_notification',
-                                   'Notification')
+                                   'Notification', required=True)
     country = fields.Many2One('country.country', 'Country', required=True)
     subdiv = fields.Many2One('country.subdivision', 'Province/State',
                              domain=[('country', '=', Eval('country'))],
                              depends=['country'])
     departure_date = fields.Date('Date of departure',
                                  help='Date departed from country visited')
+
+    _order = [('notification', 'DESC'), ('departure_date', 'DESC'),
+              ('country', 'ASC')]
