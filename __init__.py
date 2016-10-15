3ded1d2ff5f3e41099104a642a3e91f9d02f21de
Randy1Burrell <Randy1burrell@yahoo.com>
2016-10-15 11:36:44 -0500

    Added report definition for disease notifications to tryton pool

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
+"""PythoThis file tell python that this folder is a python package"""
 from trytond.pool import Pool
 from .models import (DiseaseNotification, TravelHistory, NotificationSymptom,
                      NotifiedSpecimen, GnuHealthSequences, RiskFactorCondition,
                      NotificationStateChange, LabResultType)
 from .reports import (RawDataReport, CaseCountReport, CaseCountWizard,
-                      CaseCountStartModel)
+                      CaseCountStartModel, Notifications)
 from .wizards import NotifyFromEncounter
 
 
 def register():
+    """Register models to tryton's pool"""
     Pool.register(
         GnuHealthSequences,
         DiseaseNotification,
@@ -23,6 +25,7 @@ def register():
     Pool.register(
         RawDataReport,
         CaseCountReport,
+        Notifications,
         module='health_disease_notification', type_='report')
 
     Pool.register(
