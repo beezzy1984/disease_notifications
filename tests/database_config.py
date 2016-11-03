adbf6742daa8664427380f5acde77bdaf63b81dd
Randy1Burrell <Randy1burrell@yahoo.com>
2016-11-02 22:26:41 -0500

    Modified tests/database_config.py to allow database name in os.environ['DB_NAME'] passed as a command line argument in health_jamaica/files/run_tests.py file whenever tests are ran

--- tests/database_config.py
+++ tests/database_config.py
@@ -1,7 +1,10 @@
 """Defines database configuration for testing"""
+import os
 from trytond.modules.health_jamaica.tryton_utils import test_database_config
 
 
 def set_up_datebase(config_file=None, institution_name=None, database_name=None):
     """Set up the testing databae"""
+    if not database_name:
+        database_name = os.environ['DB_NAME']
     return test_database_config(config_file, institution_name, database_name=database_name)
